26ad7fd676a81436443322b248417ee66248b595
vmbdfc <vmbdfc@gmail.com>
2025-08-19 16:28:55 -0600

    Data Fixes
    Fix incorrect module name
    Add ee2 rings to default (updated gitignore for this)

--- util/data.js
+++ util/data.js
@@ -1,6 +1,6 @@
 import PogObject from "../../PogData/index"
 
-export const data = new PogObject("Astra", {
+export const data = new PogObject("ItsJustTexting", {
     sst: {
         x: Renderer.screen.getWidth() / 2,
         y: Renderer.screen.getHeight() / 2 + 20,
@@ -13,12 +13,12 @@ export const data = new PogObject("Astra", {
     }
 }, "data/data.json")
 
-export const waypointData = new PogObject("Astra", {
+export const waypointData = new PogObject("ItsJustTexting", {
     waypoints: [],
     rings: []
 }, "data/waypoints.json")
 
-export const acData = new PogObject("Astra", {
+export const acData = new PogObject("ItsJustTexting", {
     lcItems: [],
     rcItems: []
 }, "data/autoclicker.json")
